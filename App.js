a1d0476af3207f2921d1c162dc76ff56e414732e
vivek666 <amalhan43@gmail.com>
2021-01-28 03:00:32 +0530

    Initial setup

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "./screens/LoginScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Login" component={LoginScreen} />
+			</Stack.Navigator>
+			<StatusBar style="auto" />
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyCnA3JRXqmnS79imz_4Qco8jklhwX74oO4",
+	authDomain: "signal-9c29d.firebaseapp.com",
+	projectId: "signal-9c29d",
+	storageBucket: "signal-9c29d.appspot.com",
+	messagingSenderId: "93249102423",
+	appId: "1:93249102423:web:303674910169a3b6347912",
+	measurementId: "G-ZNS3PS5J6N",
+};
