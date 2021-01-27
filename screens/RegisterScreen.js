0787daae827c29aea3c38b63da09f6c72e375eee
vivek666 <amalhan43@gmail.com>
2021-01-28 04:03:52 +0530

    Basic style implementation of Register Screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,77 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, KeyboardAvoidingView } from "react-native";
+import { View } from "react-native";
+import { Button, Input, Text } from "react-native-elements";
 
 const RegisterScreen = ({ navgation }) => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [imageUrl, setImageUrl] = useState("");
+
+	const register = () => {};
+
 	return (
-		<View>
-			<Text>I am a Register Screen</Text>
-		</View>
+		<KeyboardAvoidingView behavior="padding" enabled style={styles.container}>
+			<StatusBar style="light" />
+			<Text h3 style={{ marginBottom: 50 }}>
+				Create a Signal account
+			</Text>
+			<View style={styles.inputContainer}>
+				<Input
+					placeholder="Full Name"
+					autoFocus
+					type="text"
+					value={name}
+					onChangeText={(text) => setName(text)}
+				/>
+				<Input
+					placeholder="Email"
+					type="email"
+					value={email}
+					onChangeText={(text) => setEmail(text)}
+				/>
+				<Input
+					placeholder="Password"
+					type="text"
+					secureTextEntry
+					value={password}
+					onChangeText={(text) => setPassword(text)}
+				/>
+				<Input
+					placeholder="Image Url (optional)"
+					type="text"
+					value={imageUrl}
+					onChangeText={(text) => setImageUrl(text)}
+					onSubmitEditing={register}
+				/>
+			</View>
+			<Button
+				containerStyle={styles.button}
+				onPress={register}
+				title="Register"
+				raised
+			/>
+		</KeyboardAvoidingView>
 	);
 };
 
 export default RegisterScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 10,
+		backgroundColor: "white",
+	},
+	button: {
+		width: 200,
+		marginTop: 10,
+	},
+	inputContainer: {
+		width: 300,
+	},
+});
