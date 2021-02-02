f79611625e4adde88127b354a93a9238fe0f231d
vivek666 <amalhan43@gmail.com>
2021-02-02 20:48:36 +0530

    Image Picker Implemented

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,15 +1,44 @@
 import React, { useLayoutEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, KeyboardAvoidingView } from "react-native";
-import { View } from "react-native";
+import { View, StyleSheet, KeyboardAvoidingView, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import { Button, Input, Text } from "react-native-elements";
 import { auth } from "../firebase";
+import * as ImagePicker from "expo-image-picker";
 
 const RegisterScreen = ({ navigation }) => {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const [imageUrl, setImageUrl] = useState("");
+	const [profilePic, setProfilePic] = useState();
+
+	const getPermissions = async () => {
+		if (Platform.OS !== "web") {
+			const {
+				status,
+			} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+			if (status !== "granted") {
+				alert("We need permission to access your camera ");
+			}
+		}
+		pickImage();
+	};
+
+	const pickImage = async () => {
+		try {
+			let result = await ImagePicker.launchImageLibraryAsync({
+				mediaTypes: ImagePicker.MediaTypeOptions.All,
+				allowsEditing: true,
+				aspect: [1, 1],
+				quality: 0.5,
+			});
+			if (!result.cancelled) {
+				setProfilePic(result.uri);
+			}
+		} catch (error) {
+			console.log("Error @pickImage:", error);
+		}
+	};
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
@@ -24,8 +53,8 @@ const RegisterScreen = ({ navigation }) => {
 				authUser.user.updateProfile({
 					displayName: name,
 					photoURL:
-						imageUrl ||
-						"http://www.cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+						profilePic ||
+						"setProfilePic://www.cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
 				});
 			})
 			.catch((error) => alert(error.message));
@@ -38,6 +67,22 @@ const RegisterScreen = ({ navigation }) => {
 				Create a Signal account
 			</Text>
 			<View style={styles.inputContainer}>
+				<View style={styles.profileImage}>
+					{!profilePic ? (
+						<AntDesign
+							name="plus"
+							size={24}
+							color="#FFFF"
+							onPress={getPermissions}
+						/>
+					) : (
+						<Image
+							source={{ uri: profilePic }}
+							style={{ width: 100, height: 100 }}
+							onPress={getPermissions}
+						/>
+					)}
+				</View>
 				<Input
 					placeholder="Full Name"
 					autoFocus
@@ -48,8 +93,11 @@ const RegisterScreen = ({ navigation }) => {
 				<Input
 					placeholder="Email"
 					type="email"
+					autoCapitalize="none"
+					autoCompleteType="email"
 					value={email}
 					onChangeText={(text) => setEmail(text)}
+					keyboardType="email-address"
 				/>
 				<Input
 					placeholder="Password"
@@ -57,12 +105,6 @@ const RegisterScreen = ({ navigation }) => {
 					secureTextEntry
 					value={password}
 					onChangeText={(text) => setPassword(text)}
-				/>
-				<Input
-					placeholder="Image Url (optional)"
-					type="text"
-					value={imageUrl}
-					onChangeText={(text) => setImageUrl(text)}
 					onSubmitEditing={register}
 				/>
 			</View>
@@ -92,5 +134,17 @@ const styles = StyleSheet.create({
 	},
 	inputContainer: {
 		width: 300,
+	},
+	profileImage: {
+		backgroundColor: "#2c6bed",
+		borderRadius: 40,
+		height: 80,
+		width: 80,
+		marginTop: -20,
+		marginBottom: 10,
+		overflow: "hidden",
+		alignSelf: "center",
+		alignItems: "center",
+		justifyContent: "center",
 	},
 });
